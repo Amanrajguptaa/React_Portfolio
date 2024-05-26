ca1bff25f0cd8d664d9dbee6b292f6a48ef6f1be
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-05-26 17:18:17 +0530

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
     theme: {
-    extend: {},
+     extend: {
+      fontFamily:{
+        primaryRegular:['BOOMSTER'],
+        waterfall:['Waterfall','cursive'],
+      }
+    },
   },
   plugins: [],
 }
