9097acaf0526f036a427156e1cb266bbe1d61d4c
Aman Raj Gupta <152897024+Amanrajguptaa@users.noreply.github.com>
2024-05-26 17:17:32 +0530

    Updated Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,11 +2,13 @@ import Profile from '/src/Components/Profile/Profile.jsx'
 import About from '/src/Components/About/About.jsx'
 import Contact from '/src/Components/Contact/Contact.jsx'
 import Education from '/src/Components/Education/Education'
+import Hero from '/src/Components/Hero/Hero.jsx'
 import Skills from '/src/Components/Skills/Skills.jsx'
 import Projects from '/src/Components/Project/Projects.jsx'
 export default function Home(){
     return (
         <>
+        <Home/>
         <Profile/>
         <About/>
         <Contact/>
